39ce5cc9362a7dd92438b33cfba99a8ee312bf10
Jacqueline <jcasalia@yahoo.com.br>
2023-12-13 18:10:00 -0300

    projeto finalizado

--- src/app/meus-pedidos/components/pedido-item.tsx
+++ src/app/meus-pedidos/components/pedido-item.tsx
@@ -3,6 +3,10 @@ import { Card } from "@/components/ui/card";
 import { Prisma } from "@prisma/client";
 import { format  } from 'date-fns'
 import PedidoInformacao from "./pedido-informacao";
+import { Separator } from "@/components/ui/separator";
+import { useMemo } from "react";
+import { totalPrecoProduto } from "@/helpers/desconto";
+import { getNeusPedidosStatus } from "../helpers/status";
 // traz do prisma
 interface PedidoItemProps{
     pedido:Prisma.OrderGetPayload<{
@@ -15,7 +19,30 @@ interface PedidoItemProps{
 
 }
 const PedidoItem  = ({pedido}:PedidoItemProps) => {
-    return ( 
+// calculo subtotal
+const subtotal = useMemo(() => {
+  return pedido.orderProducts.reduce((acc, meuspedidos) => {
+    return (
+      acc + Number(meuspedidos.product.basePrice) * meuspedidos.quantity
+    );
+  }, 0);
+}, [pedido.orderProducts]);
+
+// calculo total
+const total = useMemo(() => {
+  return pedido.orderProducts.reduce((acc, orderProduct) => {
+    const productWithTotalPrice = totalPrecoProduto(
+      orderProduct.product,
+    );
+    return acc + productWithTotalPrice.totalPreco * orderProduct.quantity;
+  }, 0);
+}, [pedido]);
+
+
+
+const desconto = subtotal - total;
+
+  return ( 
   
   <Card className="px-5">
     {/* collapsible abre e fecha */}
@@ -25,7 +52,12 @@ const PedidoItem  = ({pedido}:PedidoItemProps) => {
     <AccordionTrigger>
 
 <div className="flex flex-col gap-1 text-left lg:text-xl">
-    Pedido com {pedido.orderProducts.length} produtos
+  <p>Pedido com {pedido.orderProducts.length} produtos</p>
+  
+   <span className="text-sm opacity-60">
+   Feito em {format(pedido.createdAt,"d/MM/y 'as'  HH:mm")}
+
+    </span> 
 </div>
 
     </AccordionTrigger>
@@ -34,7 +66,7 @@ const PedidoItem  = ({pedido}:PedidoItemProps) => {
    <div className="flex items-center justify-between lg:text-lg">
    <div className="font-bold">
 <p>Status</p>
-<p className="text-[#8162FF]">{pedido.status}</p>
+<p className="text-[#8162FF]">{getNeusPedidosStatus(pedido.status)}</p>
    </div>
    <div>
 <p className="font-bold">Data</p>
@@ -54,7 +86,31 @@ const PedidoItem  = ({pedido}:PedidoItemProps) => {
                 />
               ))}
 
+<div className="flex flex-col gap-1 w-full text-xs">
+  <Separator/>
+<div className="flex w-full justify-between py-3">
+ <p>Subtotal</p>
+ <p>R$ {subtotal.toFixed(2)}</p>
+</div>
 
+ <Separator/>
+<div className="flex w-full justify-between py-3">
+  <p>Entrega</p>
+  <p>Gratis</p>
+</div>
+<Separator/>
+<div className="flex w-full justify-between py-3">
+<p>Desconto</p>
+ <p>R$ {desconto.toFixed(2)}</p>
+</div>
+ <Separator/>
+<div className="flex w-full justify-between py-3 text-sm font-bold">
+<p>Total</p>
+
+ <p>R${total.toFixed(2)}</p>
+</div>
+
+</div>
 
 
    </div>
